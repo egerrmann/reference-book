050e0bb481144a377e064c5692ef5a3dec0c0e6b
Yahor <yatskoegor@gmail.com>
2023-12-08 15:31:12 +0300

    create initial java project

---
+++ src/main/java/by/bsu/referencebook/ReferenceBookApplication.java
@@ -0,0 +1,13 @@
+package by.bsu.referencebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsu/referencebook/ReferenceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.referencebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
